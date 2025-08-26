85e23923c345e5108b7fd9eb43037f71a6a1b2f0
Artem <artemzheleznovjob@gmail.com>
2025-08-26 18:20:27 +0200

    Изменения в файле template.html - добавлено временные товары. Добавлены файлы static/myapp/js/main.js. Добавлен файл для детальной информации о товаре (пока что макет)
    # Conflicts:
    #	OnlineStore/urls.py
    #	myapp/urls.py
    #	myapp/views.py

---
+++ myapp/static/myapp/js/main.js
@@ -0,0 +1,16 @@
+
+//product_detail
+//код для визуального переключения между все про товар, характеристики и отзывы
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.product-tabs-nav .tab-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            //убирает активный класс
+            navLinks.forEach(l => l.classList.remove('active'));
+
+            //доавляет актив в нужное значение (что было нажато) файл
+            this.classList.add('active');
+        });
+    });
+});
--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Product
 
-from .models import Product
-
-# --- index
 def index_page(req):
     products = Product.objects.filter(is_active=True).all()
     context = {
         'products': products
     }
     return render(req, 'myapp/template.html', context)
-
-
-# --- Category
-class CategoryCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    pass
 
