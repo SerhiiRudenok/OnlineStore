a450ac59e51eccd357eeb6b0fb20f660f2245edf
SerhiiRudenok <svaru@ukr.net>
2025-08-26 19:48:21 +0300

    Збереження перед pull

--- myapp/models.py
+++ myapp/models.py
@@ -13,9 +13,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='myapp/images/', blank=True, default='myapp/images/no-image.jpg')
-    is_active = models.BooleanField(default=True)
-
-    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .models import Product
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+from myapp.models import Product, Category
+
+
+
+# --- index
 def index_page(req):
     products = Product.objects.filter(is_active=True).all()
     context = {
@@ -8,6 +17,25 @@ def index_page(req):
     }
     return render(req, 'myapp/template.html', context)
 
+
 def product_detail_page_placeholder(request, product_id):
     return render(request, 'myapp/product_detail.html')
 
+
+# --- Category
+class CategoryCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Category
+    # form_class = CategoryForm
+    template_name = 'myapp/category/category_create.html'
+    permission_required = 'myapp.add_category'
+
+
+
+
+# --- Product
+class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Product
+    # form_class = ProductForm
+    fields = ['name', 'category', 'description', 'price', 'image']
+    template_name = 'myapp/product/product_create.html'
+    permission_required = 'myapp.add_product'
