7f3360f625e706f18d7e140454bdd88138d3c106
SerhiiRudenok <svaru@ukr.net>
2025-08-26 19:59:28 +0300

    Збереження перед pull

--- myapp/models.py
+++ myapp/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='myapp/images/', blank=True, default='myapp/images/no-image.jpg')
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
