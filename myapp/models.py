015ab3062968d0d188d0e67af06624039ebea2dc
SerhiiRudenok <svaru@ukr.net>
2025-08-31 17:09:20 +0300

    играция. одель User, Order

--- myapp/models.py
+++ myapp/models.py
@@ -84,9 +84,9 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     # доставка та оплата
-    delivery_method = models.CharField(max_length=30, choices=DELIVERY_CHOICES)
-    delivery_address = models.CharField(max_length=255)
-    payment_method = models.CharField(max_length=30, choices=PAYMENT_CHOICES)
+    delivery_method = models.CharField(max_length=30, choices=DELIVERY_CHOICES, default='nova_poshta_branch')
+    delivery_address = models.CharField(max_length=255, default='Потребує уточнення')
+    payment_method = models.CharField(max_length=30, choices=PAYMENT_CHOICES, default='cash_on_delivery')
 
     def __str__(self):
         return f"Замовлення № {self.id} від {self.user.username}"
